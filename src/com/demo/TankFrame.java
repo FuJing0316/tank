db5c2ee1f1b24aee8dd49d602de35532bcf7a2f0
FuJing0316 <582473443@qq.com>
2021-08-22 19:50:01 +0800

    把小黑块的绘制位置（坦克位置），变成变量，则每次调用paint后，坦克就可以动起来

--- src/com/demo/TankFrame.java
+++ src/com/demo/TankFrame.java
@@ -11,6 +11,9 @@ import java.awt.event.WindowEvent;
  * @Version: 1.0
  */
 public class TankFrame extends Frame {
+    int x = 200;
+    int y = 200;
+
     public TankFrame() {
         setSize(800, 600);
         setResizable(false);//设置是否可改变窗口大小
@@ -28,7 +31,11 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200, 200, 50, 50);
+        System.out.println("paint");
+        //在x=200,y=200的位置画一个黑色小块宽50高50
+        g.fillRect(x, x, 50, 50);
+        x += 10;
+        y += 10;
     }
 
 }
