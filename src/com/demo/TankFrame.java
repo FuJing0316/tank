5893deb419016d7ca15b2d99cac49f6014161922
FuJing0316 <582473443@qq.com>
2021-08-24 22:08:26 +0800

    添加键盘事件处理，根据上下左右的按键状态，判定坦克移动的方向

--- src/com/demo/TankFrame.java
+++ src/com/demo/TankFrame.java
@@ -39,20 +39,58 @@ public class TankFrame extends Frame {
         System.out.println("paint");
         //在x=200,y=200的位置画一个黑色小块宽50高50
         g.fillRect(x, y, 50, 50);
-        x+=10;//小黑块（坦克）位置移动
+        x += 10;//小黑块（坦克）位置移动
     }
 
-    class MyKeyAdapter extends KeyAdapter{
+    class MyKeyAdapter extends KeyAdapter {
+        //定义按键方向
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
             System.out.println("key pressed");
-//            x+=200;
-//            repaint();//窗口重画，配合键盘按下，坐标的变化，可以实现坦克移动效果
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
             System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
