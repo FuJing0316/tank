e1a0b7e327fef414e2769e80f61ec18e8473630b
FuJing0316 <582473443@qq.com>
2021-09-12 17:59:23 +0800

    敌人坦克随机移动

--- src/com/demo/Tank.java
+++ src/com/demo/Tank.java
@@ -92,9 +92,13 @@ public class Tank {
         }
 
         //敌人坦克随机开火
-        if (this.group == group.BAD && random.nextInt(10) > 8) {
-            this.fire();
-        }
+        if (this.group == group.BAD && random.nextInt(100) > 95) this.fire();
+        //敌人坦克移动方向随机
+        if (this.group == group.BAD && random.nextInt(100) > 95) this.randomDir();
+    }
+
+    private void randomDir() {
+        this.dir = Direction.values()[random.nextInt(4)];
     }
 
     /**
@@ -102,8 +106,8 @@ public class Tank {
      * 1、确定子弹发射出的位置，子弹方向
      */
     public void fire() {
-        int bx = this.x + 2 * ResourceMgr.bulletD.getWidth() - ResourceMgr.bulletD.getWidth() / 2;
-        int by = this.y + 2 * ResourceMgr.bulletD.getHeight() - ResourceMgr.bulletD.getHeight() / 2;
+        int bx = this.x + ResourceMgr.tankU.getWidth()/2;
+        int by = this.y + ResourceMgr.tankU.getHeight()/2;
         tf.bulltes.add(new Bullte(bx, by, dir, tf, group));
     }
 
